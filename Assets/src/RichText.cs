b062b5f20b59cef062826e7220cb29fd0a297dd2
Park Juhyung <majecty@gmail.com>
2018-11-11 13:54:01 +0900

    Initial commit

---
+++ Assets/src/RichText.cs
@@ -0,0 +1,247 @@
+﻿//  Copyright 2018 Park Juhyung <majecty@gmail.com>
+//  Permission is hereby granted, free of charge, to any person obtaining a 
+//  copy of this software and associated documentation files (the "Software"), 
+//  to deal in the Software without restriction, including without limitation 
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//  and/or sell copies of the Software, and to permit persons to whom the 
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+//  DEALINGS IN THE SOFTWARE.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RichTextSubstringHelper
+{
+    public static class StringExt
+    {
+        public static string RichTextSubString(this string text, int length)
+        {
+            var m = new RichTextSubStringMaker(text);
+            for (int i = 0; i < length; i++)
+            {
+                m.Consume();
+            }
+            return m.GetRichText();
+        }
+
+        public static int RichTextLength(this string text)
+        {
+            var m = new RichTextSubStringMaker(text);
+            var length = 0;
+            while (m.IsConsumable())
+            {
+                m.Consume();
+                length += 1;
+            }
+            return length;
+        }
+    }
+
+    class RichTextTag
+    {
+        public string tagName;
+        public string endTag
+        {
+            get
+            {
+                return "</" + tagName + ">";
+            }
+        }
+    }
+
+    public class RichTextSubStringMaker
+    {
+        private string originalText;
+        private string middleText;
+        private Stack<RichTextTag> tagStack;
+        private int consumedLength;
+
+        public RichTextSubStringMaker(string original)
+        {
+            this.originalText = original;
+            middleText = "";
+            tagStack = new Stack<RichTextTag>();
+            consumedLength = 0;
+        }
+
+        public string GetRichText()
+        {
+            if (tagStack.Count == 0)
+            {
+                return middleText;
+            }
+
+            var ret = middleText;
+            // Caution! we iterate tags from the last item of the stack.
+            // copiedQueue's first item is tagStack's last item.
+            var copiedQueue = new Queue<RichTextTag>(tagStack);
+            while (copiedQueue.Count != 0)
+            {
+                ret += copiedQueue.Dequeue().endTag;
+            }
+            return ret;
+        }
+
+        public bool IsConsumable()
+        {
+            return consumedLength < originalText.Length;
+        }
+
+        public void Consume()
+        {
+            Debug.Assert(IsConsumable());
+            var peekedOriginChar = PeekNextOriginChar();
+            if (peekedOriginChar == '<')
+            {
+                if (PeekNextNextOriginChar() == '/')
+                {
+                    ConsumeEndTag();
+                }
+                else
+                {
+                    ConsumeStartTag();
+                }
+                Consume();
+            }
+            else
+            {
+                ConsumeRawChar();
+            }
+        }
+
+        private char? PeekNextNextOriginChar()
+        {
+            if (originalText.Length <= consumedLength + 1)
+            {
+                return null;
+            }
+            return originalText[consumedLength + 1];
+        }
+
+        private char PeekNextOriginChar()
+        {
+            return originalText[consumedLength];
+        }
+
+        private void ConsumeStartTag()
+        {
+            Debug.Assert(PeekNextOriginChar() == '<');
+            var tagName = "";
+            bool tagNameComplete = false;
+
+            ConsumeRawChar();
+
+            while (true)
+            {
+                var consumedChar = ConsumeRawChar();
+                if (consumedChar == null)
+                {
+                    Debug.LogError("Cannot close start tag");
+                    return;
+                }
+
+                if (consumedChar == '>')
+                {
+                    if (tagName == "")
+                    {
+                        Debug.LogWarning("Empty tag name");
+                    }
+
+                    tagStack.Push(new RichTextTag
+                    {
+                        tagName = tagName
+                    });
+                    return;
+                }
+
+                if (!tagNameComplete)
+                {
+                    if (!char.IsLetterOrDigit(consumedChar.Value))
+                    {
+                        tagNameComplete = true;
+                    }
+                    else
+                    {
+                        tagName += consumedChar;
+                    }
+                }
+            }
+        }
+
+        private void ConsumeEndTag()
+        {
+            Debug.Assert(PeekNextOriginChar() == '<');
+            Debug.Assert(PeekNextNextOriginChar() == '/');
+            var tagName = "";
+            bool tagNameComplete = false;
+
+            ConsumeRawChar();
+            ConsumeRawChar();
+
+            while (true)
+            {
+                var consumedChar = ConsumeRawChar();
+                if (consumedChar == null)
+                {
+                    Debug.LogError("Cannot close start tag");
+                    return;
+                }
+
+                if (consumedChar == '>')
+                {
+                    if (tagName == "")
+                    {
+                        Debug.LogWarning("Empty tag name");
+                    }
+
+                    if (tagStack.Count == 0)
+                    {
+                        Debug.LogError("Could not pop tag " + tagName);
+                    }
+
+                    if (tagStack.Peek().tagName != tagName)
+                    {
+                        Debug.LogError("Could not pop tag " + tagName + " expeted " + tagStack.Peek().tagName);
+                    }
+
+                    tagStack.Pop();
+                    return;
+                }
+
+                if (!tagNameComplete)
+                {
+                    if (!char.IsLetterOrDigit(consumedChar.Value))
+                    {
+                        tagNameComplete = true;
+                    }
+                    else
+                    {
+                        tagName += consumedChar;
+                    }
+                }
+            }
+        }
+
+        private char? ConsumeRawChar()
+        {
+            if (consumedLength > originalText.Length)
+            {
+                return null;
+            }
+            var peeked = PeekNextOriginChar();
+            this.middleText += peeked;
+            this.consumedLength += 1;
+            return peeked;
+        }
+    }
+}
---
+++ Assets/test/RichTextHelperTest.cs
@@ -0,0 +1,55 @@
+﻿//  Copyright 2018 Park Juhyung <majecty@gmail.com>
+//  Permission is hereby granted, free of charge, to any person obtaining a 
+//  copy of this software and associated documentation files (the "Software"), 
+//  to deal in the Software without restriction, including without limitation 
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//  and/or sell copies of the Software, and to permit persons to whom the 
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+//  DEALINGS IN THE SOFTWARE.
+
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using RichTextSubstringHelper;
+
+public class RichTextHelperTest
+{
+    [Test]
+    public void SimpleTest() {
+        Assert.AreEqual("a".RichTextSubString(0), "");
+        Assert.AreEqual("a".RichTextSubString(1), "a");
+
+        var sampleText = "UnityTest behaves ";
+        Assert.AreEqual(sampleText.RichTextSubString(sampleText.Length), sampleText);
+    }
+
+    [Test]
+    public void TagTest()
+    {
+        var sampleTagText = "<color=#000>blah</color>x";
+        Assert.AreEqual(sampleTagText.RichTextSubString(5), sampleTagText);
+        Assert.AreEqual(sampleTagText.RichTextSubString(1), "<color=#000>b</color>");
+    }
+
+    [Test]
+    public void NestedTest()
+    {
+        var sampleTagText = "<color=#000>a<i>b<b>c</b>d</i></color>e";
+        Assert.AreEqual(sampleTagText.RichTextSubString(5), sampleTagText);
+        Assert.AreEqual(sampleTagText.RichTextSubString(1), "<color=#000>a</color>");
+        Assert.AreEqual(sampleTagText.RichTextSubString(2), "<color=#000>a<i>b</i></color>");
+        Assert.AreEqual(sampleTagText.RichTextSubString(3), "<color=#000>a<i>b<b>c</b></i></color>");
+        Assert.AreEqual(sampleTagText.RichTextSubString(4), "<color=#000>a<i>b<b>c</b>d</i></color>");
+    }
+}

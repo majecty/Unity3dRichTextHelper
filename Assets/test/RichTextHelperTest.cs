025c03bb8563d77f9fc049f418f38e516e7aaac9
LaurensMoonens <laurens32.moonens@telenet.be>
2019-12-30 18:03:42 +0100

    Added support for smaller then sign in text, without this being considered a tag starting bracket.

--- Assets/test/RichTextHelperTest.cs
+++ Assets/test/RichTextHelperTest.cs
@@ -63,4 +63,32 @@ public class RichTextHelperTest
         Assert.AreEqual(sampleTagText.RichTextSubString(3), "<color=#000>a<i>b<b>c</b></i></color>");
         Assert.AreEqual(sampleTagText.RichTextSubString(4), "<color=#000>a<i>b<b>c</b>d</i></color>");
     }
+
+    [Test]
+    public void SmallerThenSignWithoutTag()
+    {
+        var sampleTagText = "<--<color=#000>l</color><i>r</i>-->";
+        Assert.AreEqual(sampleTagText.RichTextSubString(8), sampleTagText);
+        Assert.AreEqual(sampleTagText.RichTextSubString(1), "<");
+        Assert.AreEqual(sampleTagText.RichTextSubString(2), "<-");
+        Assert.AreEqual(sampleTagText.RichTextSubString(3), "<--");
+        Assert.AreEqual(sampleTagText.RichTextSubString(4), "<--<color=#000>l</color>");
+        Assert.AreEqual(sampleTagText.RichTextSubString(5), "<--<color=#000>l</color><i>r</i>");
+        Assert.AreEqual(sampleTagText.RichTextSubString(6), "<--<color=#000>l</color><i>r</i>-");
+        Assert.AreEqual(sampleTagText.RichTextSubString(7), "<--<color=#000>l</color><i>r</i>--");
+
+        var sampleTagText2 = "<a<b<c<color=#000><</color><i>><</i>><<";
+        Assert.AreEqual(sampleTagText2.RichTextSubString(12), sampleTagText2);
+        Assert.AreEqual(sampleTagText2.RichTextSubString(1), "<");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(2), "<a");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(3), "<a<");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(4), "<a<b");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(5), "<a<b<");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(6), "<a<b<c");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(7), "<a<b<c<color=#000><</color>");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(8), "<a<b<c<color=#000><</color><i>></i>");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(9), "<a<b<c<color=#000><</color><i>><</i>");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(10), "<a<b<c<color=#000><</color><i>><</i>>");
+        Assert.AreEqual(sampleTagText2.RichTextSubString(11), "<a<b<c<color=#000><</color><i>><</i>><");
+    }
 }
